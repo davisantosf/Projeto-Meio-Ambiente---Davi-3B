02ef42f4c3d73543277fc77d0b1ccbf3b32ff290
davisantosf <160255907+davisantosf@users.noreply.github.com>
2024-06-10 07:55:08 -0300

    Update script.js

--- aula 3 e 4/script.js
+++ aula 3 e 4/script.js
@@ -6,38 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual é uma das principais causas do desmatamento na Amazônia, considerando os principais fatores econômicos e industriais que impulsionam essa prática?",
         alternativas: [
-            "Isso é assustador!",
-            "Isso é maravilhoso!"
+            "Agricultura e pecuária",
+            "Urbanização"
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Quais são os impactos do desmatamento na biodiversidade da Amazônia, levando em conta a importância ecológica da região e a interação das espécies com seu habitat?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            "Aumento da diversidade de espécies",
+            "Perda de habitat e extinção de espécies",
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Qual a consequência do desmatamento para as comunidades indígenas da Amazônia, considerando a relação dessas comunidades com a floresta e seu modo de vida tradicional?",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            "Preservação das culturas tradicionais",
+            "Deslocamento e perda de território"
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como o desmatamento na Amazônia afeta o clima global, especialmente em relação à função da floresta como sumidouro de carbono e regulador climático?",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
+            "Redução da emissão de gases de efeito estufa",
+            "Aumento da emissão de gases de efeito estufa"
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        enunciado: "Quais medidas podem ajudar a combater o desmatamento na Amazônia, considerando as políticas públicas e estratégias de conservação ambiental que podem ser implementadas?",
         alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+           "Expansão da fronteira agrícola",
+            "Fiscalização e políticas de conservação ambiental"
         ]
     },
 ];
